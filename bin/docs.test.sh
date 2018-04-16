84d81eaa9b1146f4b86a261ae23677f582db738d
Taranveer Virk <taranveer@virk.cc>
2018-04-15 20:40:03 -0400

    chore: update travis config and apply feedback
    Co-Authored-By: Raymond Feng <raymondfeng@users.noreply.github.com>

--- bin/docs.test.sh
+++ bin/docs.test.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+set -ev
+
+# Make sure we use the correct repo root dir
+DIR=`dirname $0`
+REPO_ROOT=$DIR/..
+pushd $REPO_ROOT >/dev/null
 rm -rf sandbox/loopback.io/
-git clone https://github.com/strongloop/loopback.io.git sandbox/loopback.io
+# Shadow clone is faster
+git clone --depth 1 https://github.com/strongloop/loopback.io.git sandbox/loopback.io
 npm run bootstrap
-cd sandbox/loopback.io/
+pushd $REPO_ROOT/sandbox/loopback.io/ >/dev/null
+# The following 3 lines will be replaced with npm run build
 bundle install
 npm run fetch-readmes
 bundle exec jekyll build
+popd >/dev/null
+popd >/dev/null
